39e77a982324e0ba7282b5c3ffb2edf9233c8b81
Marcelo Castellano <marce.geek22@gmail.com>
2024-06-03 21:32:05 -0300

    Comenzar código de la clase

---
+++ juego.py
@@ -0,0 +1,13 @@
+class Juego:
+
+    def __init__(self, palabra):
+        self.palabra = palabra
+
+    def arriesgar_letra(self, letra):
+        pass
+
+    def arriesgar_palabra(self, palabra):
+        pass
+
+    def intentos_disponibles(self):
+        pass
