57e66a8efa264ed10e62777e7060ae8554e4b3b1
Ignacio Roca <rocaignacio1@gmail.com>
2024-06-18 23:04:21 -0300

    Tests de letra ya usada

--- test_letra.py
+++ test_letra.py
@@ -14,22 +14,47 @@ def test_letra_invalida_progreso():
     assert j.mostrarProgresoPalabra() == "_______"
 
 def test_letra_invalida_intentos():
-    # Si la letra es invalida el nro de intentos deberia mantenerse
+    # Si la letra es invalida el nro de intentos deberia mantenerse.
     j = Juego("palabra")
     resultado = j.arriesgarLetra("#")
     assert j.intentosDisponibles() == j.maxIntentos
-    
-def test_letra_ya_usada():
-    # Si la letra ya fue usada, el nro de intentos y el avance deberian mantenerse. Y el mensaje "letra ya usada".
+
+
+# Letra ya usada
+def test_letra_ya_usada_mensaje():
+    # Si la letra ya fue usada el mensaje es "letra ya usada".
     j = Juego("palabra")
     resultado = j.arriesgarLetra("h")
     resultado = j.arriesgarLetra("H")
     assert resultado == "letra ya usada"
     
-    resultado = j.arriesgarLetra("a")
-    resultado = j.arriesgarLetra("A")
-    assert resultado == "letra ya usada"
+def test_letra_ya_usada_progreso():
+    # Si la letra es invalida el progreso deberia mantenerse.
+    j = Juego("palabra")
+    j.arriesgarLetra("a")
+    j.arriesgarLetra("A")
+    assert j.mostrarProgresoPalabra() == "_a_a__a"
     
+def test_letra_ya_usada():
+    # Si la letra es invalida el nro de intentos deberia mantenerse.
+    j = Juego("palabra")
+    j.arriesgarLetra("h")
+    j.arriesgarLetra("H")
+    assert j.intentosDisponibles() == j.maxIntentos - j.intentosLetra
+
+# Letra no esta  
+def test_letra_no_esta_mensaje():
+    # Si la letra no se encuentra, el nro de intentos deberia bajar, el avance mantenerse y el mensaje "letra no se encuentra"
+    j = Juego("palabra")
+    resultado = j.arriesgarLetra("x")
+    assert resultado == "letra no se encuentra"
+
+def test_letra_no_esta_progreso():
+    # Si la letra no se encuentra, el nro de intentos deberia bajar, el avance mantenerse y el mensaje "letra no se encuentra"
+    j = Juego("palabra")
+    resultado = j.arriesgarLetra("x")
+    assert resultado == "letra no se encuentra"
+
 def test_letra_no_esta():
     # Si la letra no se encuentra, el nro de intentos deberia bajar, el avance mantenerse y el mensaje "letra no se encuentra"
     j = Juego("palabra")
