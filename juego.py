e9412eb2e06dcead8075dc35eb70d890b0f2b662
Ignacio Roca <rocaignacio1@gmail.com>
2024-07-22 11:29:06 -0300

    Comentarios

--- juego.py
+++ juego.py
@@ -4,7 +4,7 @@ class Juego:
     intentosLetra = 1
     intentosPalabra = 2
 
-    # inicializar el juego con una palabra
+    # Se inicializa el juego con una palabra.
     def __init__(self, palabra):
         if not palabra.replace(' ', '').isalpha():
             raise ValueError("Palabra invalida: debe contener solo letras o espacios")
@@ -13,28 +13,33 @@ class Juego:
         self.intentosUsados = 0
         self.letrasUsadas = []
         
+    # Flujo del juego. El juego no terminara hasta ganar o quedarse sin intentos.
+    # Muestra el mensaje final, junto con el puntaje(intentos que sobrantes).
     def iniciarJuego(self):
         while(self.intentosDisponibles() > 0):
-            self.informacionPartida()
+            self.informacionJuego()
             print("Ingrese letra o palabra a arriesgar")
             i = input()
             
             if len(i) == 1:
                 resultado = self.arriesgarLetra(i)
                 print(resultado.upper())
-                if resultado == "ganaste":
+                if self.acerto:
                     break
             else:
                 resultado = self.arriesgarPalabra(i)
                 print(resultado.upper())
-                if resultado == "ganaste":
+                if self.acerto:
                     break
         
-    def informacionPartida(self):
-        print("Intentos restantes: ", self.intentosDisponibles())
-        print("Letras usadas: ", ' '.join(self.letrasUsadas))
-        print(self.mostrarProgresoPalabra())
+        if self.acerto:
+            print("GANASTE!")
+        else:
+            print("PERDISTE!")  
+        print(f"El puntaje fue de: {self.intentosDisponibles()}")
+        return self.intentosDisponibles()
 
+    # Dada una letra, devuelve 4 posibles valores. (letra ya usada, letra se encuentra, letra no se encuetra, letra invalida).
     def arriesgarLetra(self, letra):
         if letra.isalpha() and len(letra) == 1:
             letra = letra.lower()
@@ -44,32 +49,34 @@ class Juego:
             if letra in self.palabra:
                 if '_' not in self.mostrarProgresoPalabra():
                     self.acerto = True
-                    return "ganaste"
                 return "letra se encuentra"
             else:
-                if self.intentosDisponibles()<=0:
-                    return "pierde"
-                else:
-                    self.intentosUsados += self.intentosLetra
-                    return "letra no se encuentra"
+                self.intentosUsados += self.intentosLetra
+                return "letra no se encuentra"
         else:
             return "letra invalida"
 
+    # Dada una palabra, devuelve 2 posibles valores. (palabra correcta/palabra incorrecta).
     def arriesgarPalabra(self, palabra):
         if palabra.lower() == self.palabra:
             for i in palabra:
                 self.letrasUsadas.append(i)
             self.acerto = True
-            return "ganaste"
+            return "palabra correcta"
         self.intentosUsados += self.intentosPalabra
-        if self.intentosDisponibles() == 0:
-            return "perdiste"
-        else:
-            return "palabra incorrecta"
+        return "palabra incorrecta"
 
+    # Retorna los intentos disponibles.
     def intentosDisponibles(self):
         return max(self.maxIntentos - self.intentosUsados, 0)
     
+    # Muestra la informacion del juego actual. Intentos restantes, letra usadas y el progreso de la palabra.
+    def informacionJuego(self):
+        print("Intentos restantes: ", self.intentosDisponibles())
+        print("Letras usadas: ", ' '.join(self.letrasUsadas))
+        print(self.mostrarProgresoPalabra())
+    
+    # Retorna el progreso de la palabra.
     def mostrarProgresoPalabra(self):
         avance = ''
         for i in self.palabra:
@@ -78,6 +85,8 @@ class Juego:
             elif i == ' ':
                 avance = avance + ' '
             else:
-                avance = avance + '_'
-                
+                avance = avance + '_'      
         return avance
+    
+#j = Juego("palabra")
+#j.iniciarJuego()
