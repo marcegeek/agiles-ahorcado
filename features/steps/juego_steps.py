3774119595c15f753e2dfbba198067f2f4666a7f
Marcelo Castellano <marce.geek22@gmail.com>
2024-09-09 18:03:32 -0300

    Hacer ATDD analizando respuestas de Flask

--- features/steps/juego_steps.py
+++ features/steps/juego_steps.py
@@ -1,10 +1,22 @@
-from behave import given, when, then
-from juego import Juego
+import re
+
+from behave import given, then, when
+
+from app import app
+
+
+def get(context, action, params=None):
+    return context.client.get(action, params=params, follow_redirects=True)
+
+
+def post(context, action, data=None):
+    return context.client.post(action, data=data, follow_redirects=True)
 
 
 @given('I have a valid word "{word}"')
 def step_impl(context, word):
-    context.juego = Juego(palabra=word)
+    context.client = app.test_client()
+    context.resp = post(context, "/juego", {"palabra": word})
 
 
 @when("I start the game")
@@ -14,24 +26,37 @@ def step_impl(context):
 
 @then('I should see the masked word "{expected_progress}"')
 def step_impl(context, expected_progress):
-    assert context.juego.mostrarProgresoPalabra() == expected_progress
+    assert context.resp.status_code == 200
+    # eliminar comentarios
+    html = re.sub('<!--.*?-->', '', context.resp.text)
+    m = re.search(r"<p\b.*?>Progreso de la palabra: (.+)</p>", html, re.IGNORECASE)
+    progress = m.group(1)
+    assert progress == expected_progress
 
 
 @when('I guess the letter "{letter}"')
 def step_impl(context, letter):
-    context.resultado = context.juego.arriesgarLetra(letter)
+    context.resp = post(context, "/juego", {"intento": letter})
 
 
 @then('I should see "{expected_progress}"')
 def step_impl(context, expected_progress):
-    assert context.juego.mostrarProgresoPalabra() == expected_progress
+    assert context.resp.status_code == 200
+    html = re.sub('<!--.*?-->', '', context.resp.text)
+    m = re.search(r"<p\b.*?>Progreso de la palabra: (.+)</p>", html, re.IGNORECASE)
+    progress = m.group(1)
+    assert progress == expected_progress
 
 
 @when('I guess the word "{word}"')
 def step_impl(context, word):
-    context.resultado = context.juego.arriesgarPalabra(word)
+    context.resp = post(context, "/juego", {"intento": word})
 
 
 @then("the remaining attempts should be {expected_attempts:d}")
 def step_impl(context, expected_attempts):
-    assert context.juego.intentosDisponibles() == expected_attempts
+    assert context.resp.status_code == 200
+    html = re.sub('<!--.*?-->', '', context.resp.text)
+    m = re.search(r"<p\b.*?>Intentos restantes: (\d+)</p>", html, re.IGNORECASE)
+    intentos = int(m.group(1))
+    assert intentos == expected_attempts
