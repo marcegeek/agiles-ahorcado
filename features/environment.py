f4c1b4db195b911d16e2499e47f652d522ee68b4
Marcelo Castellano <marce.geek22@gmail.com>
2024-09-23 20:50:55 -0300

    Mejorar y unificar pasos de ATDD

---
+++ features/environment.py
@@ -0,0 +1,27 @@
+import re
+
+
+def eliminar_comentarios(html):
+     return re.sub(r"<!--.*?-->", "", html, flags=re.DOTALL)
+
+
+def get(context, action=None, params=None):
+    if action is None:
+        context.resp = context.client.get(context.url, params=params, follow_redirects=True)
+    else:
+        context.resp = context.client.get(action, params=params, follow_redirects=True)
+    assert context.resp.status_code == 200
+    context.html = eliminar_comentarios(context.resp.text)
+
+
+def post(context, action=None, data=None):
+    if action is None:
+        context.resp = context.client.post(context.url, data=data, follow_redirects=True)
+    else:
+        context.resp = context.client.post(action, data=data, follow_redirects=True)
+    assert context.resp.status_code == 200
+    context.html = eliminar_comentarios(context.resp.text)
+
+
+def arriesgar(context, intento):
+    post(context, data={"intento": intento})
